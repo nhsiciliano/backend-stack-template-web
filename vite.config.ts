3f20846ac0c845dd6517ba204c25498449107efa
Nico Siciliano <97644782+nhsiciliano@users.noreply.github.com>
2026-05-12 13:44:15 -0300

    scaffold: TanStack Start + Cloudflare + tanstack-query base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    devtools(),
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+})
+
+export default config
